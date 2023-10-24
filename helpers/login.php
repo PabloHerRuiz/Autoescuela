2335e2a88fe941d0a0fd4d81161e55bb440ab3b8
PabloHerRuiz <pherrui680@g.educaand.es>
2023-10-24 13:15:53 +0200

    agregados los helpers de login y sesion

---
+++ helpers/login.php
@@ -0,0 +1,15 @@
+<?php
+class Login
+{
+    //funcion que inicia sesion del usuario
+    public function user_login()
+    {
+
+    }
+    //funcion que cierra la sesion del usuario
+    public function user_logout()
+    {
+
+    }
+}
+?>
