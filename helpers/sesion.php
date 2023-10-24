7ed10d4a3ecb114bef84c99145478e805dc48659
PabloHerRuiz <pherrui680@g.educaand.es>
2023-10-24 13:29:07 +0200

    poner las funciones publicas

--- helpers/sesion.php
+++ helpers/sesion.php
@@ -2,24 +2,24 @@
 class Sesion
 {
     //inicia sesion
-    function iniciar_sesion()
+    public function iniciar_sesion()
     {
         session_start();
     }
     //destruye la sesion
-    function cerrar_session()
+    public function cerrar_session()
     {
         session_destroy();
     }
 
     //guarda una sesion por su clave valor
-    function guardar_sesion($clave, $valor)
+    public function guardar_sesion($clave, $valor)
     {
         $_SESSION[$clave] = $valor;
     }
 
     //muestra un valor segun su clave
-    function leer_sesion($clave)
+    public function leer_sesion($clave)
     {
         if (isset($_SESSION[$clave])) {
             return $_SESSION[$clave];
@@ -30,13 +30,13 @@ class Sesion
     }
 
     //comprueba si existe valor
-    function esta_logueado()
+    public function esta_logueado()
     {
         return isset($_SESSION['usuario']);
     }
 
     //inicia y guarda el valor
-    function login_sesion($valor)
+    public function login_sesion($valor)
     {
         iniciar_sesion();
         guardar_sesion('usuario', $valor);
