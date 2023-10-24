fc1f445c82eead083bff0e976b47f50448d4bfc1
PabloHerRuiz <145333119+PabloHerRuiz@users.noreply.github.com>
2023-10-24 12:51:11 +0200

    Add files via upload

---
+++ database/db.php
@@ -0,0 +1,19 @@
+<?php
+class DB {
+
+private static $conexion=null;
+public static function abreConexion()
+{
+    if (self::$conexion === null) {
+        try {
+            $conexion = new PDO('mysql:host=localhost;dbname=autoescuela', 'pablo', '1234');
+            $version = $conexion->getAttribute(PDO::ATTR_SERVER_VERSION);
+            echo "Versión: $version";
+        } catch (PDOException $e) {
+            echo "Error de conexión: " . $e->getMessage();
+        }
+    }
+    return $conexion;
+}
+}
+?>
---
+++ helpers/sesion.php
@@ -0,0 +1,42 @@
+<?php
+//inicia sesion
+function iniciar_sesion()
+{
+    session_start();
+}
+//destruye la sesion
+function cerrar_session()
+{
+    session_destroy();
+}
+
+//guarda una sesion por su clave valor
+function guardar_sesion($clave, $valor)
+{
+    $_SESSION[$clave] = $valor;
+}
+
+//muestra un valor segun su clave
+function leer_sesion($clave)
+{
+    if(isset($_SESSION[$clave])){
+        return $_SESSION[$clave];
+    }else{
+        return "";
+    }
+    
+}
+
+//comprueba si existe valor
+function esta_logueado()
+{
+    return isset($_SESSION['usuario']);
+}
+
+//inicia y guarda el valor
+function login_sesion($valor)
+{
+    iniciar_sesion();
+    guardar_sesion('usuario', $valor);
+}
+?>
