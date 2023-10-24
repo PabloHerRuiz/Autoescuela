d841010b7fff49df9ac20db5e4e34bc43c93713c
PabloHerRuiz <pherrui680@g.educaand.es>
2023-10-24 13:07:16 +0200

    cambios en validator

---
+++ helpers/validator.php
@@ -0,0 +1,21 @@
+<?php
+class Validator
+{
+    //funcion que valida un email
+    public function validaEmail($email)
+    {
+    }
+    //funcion que valida el rango de edad valido
+    public function validaEdad($clave, $valor, $min, $max)
+    {
+    }
+    //funcion que comprueba si hay errores
+    public function hayErrores()
+    {
+    }
+    //funcion que devuelve un array de errores
+    public function getErrores(){
+
+    }
+}
+?>
