a2872cc395c13c4f7f50bdaee071074a4df72956
PabloHerRuiz <pherrui680@g.educaand.es>
2023-10-25 08:34:42 +0200

    formateo del codigo

--- database/db.php
+++ database/db.php
@@ -1,19 +1,20 @@
 <?php
-class DB {
-
-private static $conexion=null;
-public static function abreConexion()
+class DB
 {
-    if (self::$conexion === null) {
-        try {
-            $conexion = new PDO('mysql:host=localhost;dbname=autoescuela', 'pablo', '1234');
-            $version = $conexion->getAttribute(PDO::ATTR_SERVER_VERSION);
-            echo "Versión: $version";
-        } catch (PDOException $e) {
-            echo "Error de conexión: " . $e->getMessage();
+
+    private static $conexion = null;
+    public static function abreConexion()
+    {
+        if (self::$conexion === null) {
+            try {
+                $conexion = new PDO('mysql:host=localhost;dbname=autoescuela', 'pablo', '1234');
+                $version = $conexion->getAttribute(PDO::ATTR_SERVER_VERSION);
+                echo "Versión: $version";
+            } catch (PDOException $e) {
+                echo "Error de conexión: " . $e->getMessage();
+            }
         }
+        return $conexion;
     }
-    return $conexion;
-}
 }
 ?>
