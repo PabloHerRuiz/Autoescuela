e0ef463610f6c115d2ef4bb334400aa3bf4a09be
PabloHerRuiz <pherrui680@g.educaand.es>
2023-10-30 10:53:35 +0100

    repositorio

--- repositorios/userRepository.php
+++ repositorios/userRepository.php
@@ -3,7 +3,7 @@ class UserRepository
 {
     private $conexion = db::abreConexion;
     //CREAR
-    public function creaUser($nombre, $password, $rol)
+    public function createUser($nombre, $password, $rol)
     {
         $query = "INSERT INTO USER (nombre,password,rol) values (:nombre,:password,:rol)";
         $stmt = $this->conexion->prepare($query);
