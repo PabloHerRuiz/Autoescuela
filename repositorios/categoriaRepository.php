495a281aab1c949b3068cb52bd20aed15e78ae25
PabloHerRuiz <pherrui680@g.educaand.es>
2023-10-30 11:57:24 +0100

    CRUD REPOSITORIOS

---
+++ repositorios/categoriaRepository.php
@@ -0,0 +1,34 @@
+<?php
+class CategoriaRepository{
+    private $conexion = db::abreConexion;
+
+    //CREAR
+    public function createCategoria($idCategoria,$nombre){
+        $query="INSERT INTO CATEGORIA (idCategoria,nombre) VALUES (:idCategoria,:nombre)";
+        $stmt=$this->conexion->prepare($query);
+        $stmt->bindParam(":idCategoria", $idCategoria, PDO::PARAM_INT);
+        $stmt->bindParam(":nombre", $nombre, PDO::PARAM_STR);
+        $stmt->execute();
+
+    }
+        
+    //BORRAR
+    public function deleteCategoria($idCategoria){
+        $query= "DELETE CATEGORIA WHERE IDCATEGORIA=:idCategoria";
+        $stmt=$this->conexion->prepare($query);
+        $stmt->bindParam(":idCategoria", $idCategoria, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+
+    //UPDATE
+    // public function updateCategoria($idCategoria){}
+
+    //LEER
+    public function readCategoria($idCategoria){
+        $query= "SELECT * FROM CATEGORIA WHERE IDCATEGORIA=:idCategoria";
+        $stmt=$this->conexion->prepare($query);
+        $stmt->bindParam(":idCategoria", $idCategoria, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+}
+?>
