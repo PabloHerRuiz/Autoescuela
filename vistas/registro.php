5066fda79242e688bc831ad67fa3751f7403da4d
PabloHerRuiz <pherrui680@g.educaand.es>
2023-11-05 20:04:42 +0100

    titulos

--- vistas/registro.php
+++ vistas/registro.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://virtual.localpablo.com/Autoescuela/css/estilosRegistro.css">
-    <title>Document</title>
+    <title>Registro de Usuario</title>
 </head>
 
 <body>
