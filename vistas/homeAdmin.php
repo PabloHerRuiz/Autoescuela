2703d18569e42bb9b8e1e39352e128e26b8ec3c3
PabloHerRuiz <pherrui680@g.educaand.es>
2023-11-06 10:22:19 +0100

    nueva vista

---
+++ vistas/homeAdmin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>HOME</title>
+    <link rel="stylesheet" href="http://virtual.localpablo.com/Autoescuela/css/estilosHomeAdmin.css">
+</head>
+
+<body>
+    <?php
+    require_once $_SERVER["DOCUMENT_ROOT"] . '/Autoescuela/helpers/autocargador.php';
+
+    ?>
+    <div class="navbar">
+        <img src="/Autoescuela/css/imagenes/logo.png" alt="Logo">
+        <h1>HOME</h1>
+        <div class="icons">
+            <img src="/Autoescuela/css/imagenes/usuario.png" alt="Perfil">
+            <img src="/Autoescuela/css/imagenes/configuraciones.png" alt="Ajustes">
+        </div>
+    </div>
+    <div class="content">
+        <div class="button-container">
+            <button>Crear Test</button>
+            <button>Listado Test</button>
+            <button>Modificar Test</button>
+            <button>Listado Alumnos</button>
+        </div>
+    </div>
+</body>
+
+</html>
