972d9c605ce8ea464c27993c876c8aaa1de38d9d
PabloHerRuiz <pherrui680@g.educaand.es>
2023-11-07 13:19:47 +0100

    el cargador siempre encima

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ class Principal
 {
     public static function main()
     {
-        require_once $_SERVER["DOCUMENT_ROOT"] . '/vistas/enruta.php'; 
         require_once $_SERVER["DOCUMENT_ROOT"] . '/helpers/autocargador.php'; 
+        require_once $_SERVER["DOCUMENT_ROOT"] . '/vistas/enruta.php'; 
     }
 }
 Principal::main();
