f54135c2536477b1270d109c359a412b394bdbbf
PabloHerRuiz <pherrui680@g.educaand.es>
2023-11-07 11:00:48 +0100

    cerrar sesion

--- vistas/homeAlu.php
+++ vistas/homeAlu.php
@@ -4,20 +4,21 @@
 <head>
     <title>HOME</title>
     <link rel="stylesheet" href="http://virtual.localpablo.com/Autoescuela/css/estilosHomeAlu.css">
+    
 </head>
 
 <body>
     <?php
     require_once $_SERVER["DOCUMENT_ROOT"] . '/Autoescuela/helpers/autocargador.php';
-
+    Sesion::iniciar_sesion();
     ?>
     <div class="navbar">
         <img src="/Autoescuela/css/imagenes/logo.png" alt="Logo">
         <h1>HOME</h1>
         <div class="icons">
-            <img src="/Autoescuela/css/imagenes/usuario.png" alt="Perfil">
-            <img src="/Autoescuela/css/imagenes/configuraciones.png" alt="Ajustes">
-            <img src="/Autoescuela/css/imagenes/cerrar-sesion.png" alt="Cierra sesion">
+            <img id="perfil" src="/Autoescuela/css/imagenes/usuario.png" alt="Perfil">
+            <img id="ajustes" src="/Autoescuela/css/imagenes/configuraciones.png" alt="Ajustes">
+            <img id="cerrar" src="/Autoescuela/css/imagenes/cerrar-sesion.png" alt="Cierra sesion">
         </div>
     </div>
     <div class="content">
@@ -27,6 +28,7 @@
             <button>Generar Test</button>
         </div>
     </div>
+    <script src="http://virtual.localpablo.com/Autoescuela/js/nav.js"></script>
 </body>
 
 </html>
